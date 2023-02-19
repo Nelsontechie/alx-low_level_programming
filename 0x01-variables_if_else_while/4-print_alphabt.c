89ad0a4c7fac3f4cce4c90bc6063f9f866809e8b
Nelsontechie <nelsonokondu@gmail.com>
2023-02-18 17:54:42 -0800

    exception of some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the main function
+ *
+ * Return: this is a constant 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+		putchar('\n');
+		return (0);
+}
