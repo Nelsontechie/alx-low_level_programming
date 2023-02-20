893226f496cb416f223045d0df8291518174a9b9
Nelsontechie <nelsonokondu@gmail.com>
2023-02-20 01:29:24 -0800

    wrong code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the function
+ *
+ * Return: this is the return value
+ *
+ * 1 - this is the return value
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19""\n");
+	return (1);
+}
