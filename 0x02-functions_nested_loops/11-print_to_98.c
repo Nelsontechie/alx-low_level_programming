0827e15ea666907f5620612f3e4e608d2a87308d
Nelsontechie <nelsonokondu@gmail.com>
2023-02-21 17:36:29 -0800

    multiple

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - this is the main function
+ *
+ * @n: this is the variable
+ */
+
+void print_to_98(int n)
+{
+	int i;
+	{
+		if (n <= 98)
+		{
+			for (i = n; i < 98; i++)
+	{
+		printf("%d, ", i);
+	}
+		}
+		else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98\n");
+	}
+}
