f6a6c94cd67a78fdeb7c680f4be86a17bf35d628
Nelsontechie <nelsonokondu@gmail.com>
2023-02-23 11:33:45 -0800

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - the function
+ */
+
+void print_most_numbers(void)
+{
+	char numbers[] = "01356789\n";
+	int i;
+
+	for (i = 0; i < numbers[i]; i++)
+	{
+		if (numbers[i] != '2' && numbers[i] != '4')
+		{
+			_putchar(numbers[i]);
+		}
+	}
+}
