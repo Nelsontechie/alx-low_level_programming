ec546a7e37071453bc1b70ff62b1a8a193d63f4b
Nelsontechie <nelsonokondu@gmail.com>
2023-02-24 13:31:02 -0800

    print updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,7 +8,7 @@ void print_numbers(void)
 	char nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\n'};
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 11; i++)
 	{
 		_putchar(nums[i]);
 	}
